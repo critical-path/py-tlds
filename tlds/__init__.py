9a215de33ce53e828fd316d1a1c84b2fe15420a6
critical-path <37241479+critical-path@users.noreply.github.com>
2018-07-28 22:20:19 -0400

    Update __init__.py

--- tlds/__init__.py
+++ tlds/__init__.py
@@ -1,7 +1,10 @@
+"""util that retrieves and validates a list of top-level domains from the internet assigned names authority."""
+
+
 from tlds.tlds_lib import TopLevelDomainGetter
+
 from tlds.tlds_utils import write_results
 
-"util that retrieves and validates a list of top-level domains from the internet assigned names authority."
 
 __version__ = "1.0.0"
 
