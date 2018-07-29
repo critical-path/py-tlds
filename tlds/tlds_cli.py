6b2525d0927ec6b13faf5f9d8d18b0336d92f176
critical-path <37241479+critical-path@users.noreply.github.com>
2018-07-28 22:43:50 -0400

    Update tlds_cli.py

--- tlds/tlds_cli.py
+++ tlds/tlds_cli.py
@@ -13,10 +13,11 @@ from tlds.tlds_lib import TopLevelDomainGetter
 
 from tlds.tlds_utils import write_results
 
+
 @command()
 @option("--write", "-w", is_flag=True, help="Write results to disk")
 def get_tlds(write=False):
-    """Util that retrieves and validates a list of top-level domains 
+    """Util that retrieves and validates a list of top-level domains
     from the Internet Assigned Names Authority."""
 
     tld_getter = TopLevelDomainGetter()
