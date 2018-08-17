3a866bc65c6b2fa10f2c2c60c1f2ab88da55d9a3
critical-path <37241479+critical-path@users.noreply.github.com>
2018-08-17 18:43:12 -0400

    On branch master  Changes to be committed:
    	modified:   tlds/lib.py

--- tlds/lib.py
+++ tlds/lib.py
@@ -18,35 +18,34 @@ from tlds.err import ValidationError
 
 class TopLevelDomainGetter(object):
     """Retrieves and validates a list of top-level domains (TLDs)
-    from the Internet Assigned Names Authority (IANA).
+       from the Internet Assigned Names Authority (IANA).
 
-    Returns
-    -------
-    self.results : ordered dict
-        Contains version number, last update, and valid TLDs
-    """
+       Returns
+       -------
+       self.results : ordered dict
+           Contains version number, last update, and valid TLDs."""
 
     def __init__(self):
         """Instantiates TopLevelDomainGetter."""
 
-        self.__urls__ = {
+        self._urls = {
             "tld": "http://data.iana.org/TLD/tlds-alpha-by-domain.txt",
             "md5": "http://data.iana.org/TLD/tlds-alpha-by-domain.txt.md5"
         }
 
-        self.__data__ = {
+        self._data = {
             "tld": None,
             "md5": None
         }
 
-        self.__digests__ = {
+        self._digests = {
             "actual": None,
             "expected": None
         }
 
-        self.__is_valid__ = False
+        self._is_valid = False
 
-        self.__patterns__ = {
+        self._patterns = {
             "other": compile("Version [A-Za-z0-9,: ]{1,}"),
             "tld": compile("[A-Z0-9-]")
         }
@@ -57,85 +56,80 @@ class TopLevelDomainGetter(object):
             "tlds": []
         })
 
-    def __get_tld_data__(self):
-        """Sends HTTP GET request to IANA.
-        Retrieves TLD data."""
+    def _get_tld_data(self):
+        """Sends HTTP GET request to IANA.  Retrieves TLD data."""
 
-        response = get(self.__urls__["tld"])
+        response = get(self._urls["tld"])
         response.raise_for_status()
 
         response_body = response.text
-        self.__data__["tld"] = response_body
+        self._data["tld"] = response_body
 
-    def __get_md5_data__(self):
-        """Sends HTTP GET request to IANA.
-        Retrieves MD5 data."""
+    def _get_md5_data(self):
+        """Sends HTTP GET request to IANA.  Retrieves MD5 data."""
 
-        response = get(self.__urls__["md5"])
+        response = get(self._urls["md5"])
         response.raise_for_status()
 
         response_body = response.text
-        self.__data__["md5"] = response_body
+        self._data["md5"] = response_body
 
-    def __get_actual_digest__(self):
+    def _get_actual_digest(self):
         """Computes MD5 digest from retrieved TLD data."""
 
-        data = bytes(self.__data__["tld"], "utf-8")
+        data = bytes(self._data["tld"], "utf-8")
         digest = md5(data).hexdigest()
-        self.__digests__["actual"] = digest
+        self._digests["actual"] = digest
 
-    def __get_expected_digest__(self):
+    def _get_expected_digest(self):
         """Extracts MD5 digest from retrieved MD5 data."""
 
-        data = self.__data__["md5"].split()
+        data = self._data["md5"].split()
         digest = data[0]
-        self.__digests__["expected"] = digest
+        self._digests["expected"] = digest
 
-    def __compare_digests__(self):
+    def _compare_digests(self):
         """Compares MD5 digests."""
 
-        if self.__digests__["actual"] == self.__digests__["expected"]:
-            self.__is_valid__ = True
+        if self._digests["actual"] == self._digests["expected"]:
+            self._is_valid = True
 
-    def __get_tlds__(self):
-        """Parses TLD data, looking for version number,
-       last update, and valid TLDs."""
+    def _get_tlds(self):
+        """Parses TLD data, looking for version number, 
+           last update, and valid TLDs."""
 
-        data = self.__data__["tld"].split("\n")
+        data = self._data["tld"].split("\n")
 
         for datum in data:
-            if search(self.__patterns__["other"], datum):
-                other = search(self.__patterns__["other"], datum).group()
-                other = other.split(",")
-                version = other[0].strip()
-                updated = other[1].strip()
+            if search(self._patterns["other"], datum):
+                other = search(self._patterns["other"], datum).group().split(",")
+                version, updated = other
 
-                self.results["version"] = version
-                self.results["updated"] = updated
+                self.results["version"] = version.strip()
+                self.results["updated"] = updated.strip()
 
-            if match(self.__patterns__["tld"], datum):
+            if match(self._patterns["tld"], datum):
                 self.results["tlds"].append(datum)
 
     def get(self):
         """Calls private methods to get, validate, and parse data.
-        Raises ValidationError if MD5 digests differ.
+           Raises ValidationError if MD5 digests differ.
 
-        Returns
-        -------
-        self.results : ordered dict
-            Contains version number, last update, and valid TLDs
-        """
+           Returns
+           -------
+           self.results : ordered dict
+               Contains version number, last update, and valid TLDs."""
 
-        self.__get_tld_data__()
-        self.__get_md5_data__()
+        self._get_tld_data()
+        self._get_md5_data()
 
-        self.__get_actual_digest__()
-        self.__get_expected_digest__()
-        self.__compare_digests__()
+        self._get_actual_digest()
+        self._get_expected_digest()
+        self._compare_digests()
 
-        if self.__is_valid__:
-            self.__get_tlds__()
+        if self._is_valid:
+            self._get_tlds()
             return self.results
 
         else:
-            raise ValidationError("Our MD5 digest ({}) did not match that provided by IANA ({}).  Please try again.".format(self.__digests__["actual"], self.__digests__["expected"]))
+            raise ValidationError("Our MD5 digest ({}) did not match that provided by IANA ({}).  Please try again.".format(self._digests["actual"], self._digests["expected"]))
